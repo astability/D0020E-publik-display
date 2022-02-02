16ae22ac206666b81353a80057172ab05b649d96
Ameer Alkadhimi <73893555+AmeerAlkadhimi20@users.noreply.github.com>
2022-02-02 12:24:56 +0100

    Add files via upload

---
+++ fibarocs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace mqtttest
+{
+    internal class fibarocs
+    {
+        static async Task Main(string[] args)
+        {
+            using var client = new HttpClient();
+
+            var result = await client.GetAsync("http://130.240.114.44/");
+            Console.WriteLine(result.Content);
+        }
+    }
+}

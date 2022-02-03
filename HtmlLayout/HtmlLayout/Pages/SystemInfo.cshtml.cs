2621c7f234ebe57417658f0ce4c8bab5a40516ec
Emil Lindström <emillindstrom@pub92-155.pub.ltu.se>
2022-02-03 14:14:04 +0100

    some basics done

---
+++ HtmlLayout/HtmlLayout/Pages/SystemInfo.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PublikDisplay.Pages
+{
+    public class SystemInfoModel : PageModel
+    {
+        public string message;
+        public void OnGet([FromRoute] int id)
+        {
+            message = id.ToString();
+        }
+    }
+}

0d3f6abe92ab05bd538c46c7b21545971fbcbad3
astability <johsdu-9@student.ltu.se>
2022-02-16 16:52:58 +0100

    Add missing enums

--- HtmlLayout/HtmlLayout/DatabaseShared.cs
+++ HtmlLayout/HtmlLayout/DatabaseShared.cs
@@ -1,6 +1,25 @@
 ﻿namespace DatabaseShared
 {
-    
+    /// <summary>
+    /// For use by database field <c>role</c>,.
+    /// </summary>
+    public enum DbEnumRole
+    {
+        Staff,
+        Admin
+    }
+
+    /// <summary>
+    /// For use by database field <c>type</c> (as part of a log object).
+    /// </summary>
+    public enum DbEnumType
+    {
+        OneTime,
+        Ongoing,
+        Closed,
+        Hidden
+    }
+
     /// <summary>
     /// For use by database fields <c>deviceStatus</c>, <c>systemStatus</c>.
     /// </summary>

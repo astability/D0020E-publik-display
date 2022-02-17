0eef69053f102ac49d45137dee40b8099a1a9dcd
Emil Lindström <emillindstrom@pub92-14.pub.ltu.se>
2022-02-17 14:04:42 +0100

    fixed settings cs and added basic slideshow

--- HtmlLayout/HtmlLayout/Pages/Index.cshtml.cs
+++ HtmlLayout/HtmlLayout/Pages/Index.cshtml.cs
@@ -5,21 +5,40 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using System.IO;
+using System.Web;
 
 namespace HtmlLayout.Pages
 {
     public class IndexModel : PageModel
     {
+        public string[] slideshow;
+        public string[] texts;
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
+
         }
 
         public void OnGet()
         {
+            slideshow = new string[] { "ALabb1.jpg", "ALabb2.jpg", "ALabb3.jpg",
+                "ALabb4.jpg", "ALabb5.jpg" };
+            texts = new string[] { "ALabb1.txt", "ALabb2.txt", "ALabb3.txt", "ALabb4.txt", "ALabb5.txt" };
+            for (var i = 0; i < texts.Length; i++)
+            {
+                var filePath = "~/imageText/" + texts[i];
+                if (System.IO.File.Exists(filePath))
+                {
 
+                }
+                else
+                {
+
+                }
+            }
         }
     }
 }
--- HtmlLayout/HtmlLayout/wwwroot/js/site.js
+++ HtmlLayout/HtmlLayout/wwwroot/js/site.js
@@ -1,7 +1,28 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Write your Javascript code.
+
+// Home page / Slideshow
+var slideIndex = 1;
+showSlide(slideIndex)
+
+function moveToSlide(n) {
+    showSlide(slideIndex += n);
+}
+
+function showSlide(n) {
+    var i;
+    var slides = document.getElementsByClassName("slides");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex - 1].style.display = "block"
+}
+
+
+// Systeminfo logs 
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
